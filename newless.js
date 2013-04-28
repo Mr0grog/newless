16f54c3e98996c8ca6b4e92149a64eef21d6ac78
Rob Brackett <rob@robbrackett.com>
2013-04-28 02:43:25 -0700

    Version 0.1 of newless

---
+++ newless.js
@@ -0,0 +1,43 @@
+(function(global) {
+  "use strict";
+
+  // ulta-simple Object.create polyfill (only does half the job)
+  var create = Object.create || (function() {
+    var Maker = function(){};
+    return function(prototype) {
+      Maker.prototype = prototype;
+      return new Maker();
+    }
+  }());
+
+  var newless = function(constructor) {
+    // in order to preserve constructor name, use the Function constructor
+    var newlessConstructor = Function("constructor, create",
+      "var newlessConstructor = function " + constructor.name + "() {" +
+        "var obj = this;" +
+        // don't create a new object if we've already got one
+        // (e.g. we were called with `new`)
+        "if (!(this instanceof newlessConstructor)) {" +
+          "obj = create(newlessConstructor.prototype);" +
+        "}" +
+        // run the original constructor
+        "var returnValue = constructor.apply(obj, arguments);" +
+        // if we got back an object (and not null), use it as the return value
+        "return (typeof returnValue === 'object' && returnValue) || obj;" +
+      "};" +
+      "return newlessConstructor;")(constructor, create);
+    newlessConstructor.prototype = constructor.prototype;
+    newlessConstructor.prototype.constructor = newlessConstructor;
+    newlessConstructor.displayName = constructor.displayName;
+    return newlessConstructor;
+  };
+
+  // support Node and browser
+  if (typeof module !== "undefined") {
+    module.exports = newless;
+  }
+  else {
+    global.newless = newless;
+  }
+
+}(this));
---
+++ test.js
@@ -0,0 +1,65 @@
+// if testing via the CLI, use the --require options to grab the helper:
+// `mocha --require test_helper_node.js`
+
+describe("newless", function() {
+  it("should make a constructor work without `new`", function() {
+    var Construct = newless(function() {});
+    var obj = Construct();
+
+    expect(obj).to.be.a(Construct);
+  });
+
+  it("should still work with `new`", function() {
+    var Construct = newless(function() {});
+    var obj = new Construct();
+
+    expect(obj).to.be.a(Construct);
+  });
+
+  it("should run the constructor's logic", function() {
+    var Construct = newless(function() {
+      this.called = true;
+    });
+    var obj = Construct();
+    
+    expect(obj.called).to.equal(true);
+  });
+
+  it("should pass arguments through", function() {
+    var Construct = newless(function(first) {
+      this.first = first;
+      // even if not in the signature
+      this.second = arguments[1];
+    });
+    var obj = Construct("a", "b");
+    
+    expect(obj.first).to.equal("a");
+    expect(obj.second).to.equal("b");
+  });
+
+  it("should preserve a constructor's prototype", function() {
+    var BareConstructor = function() {};
+    BareConstructor.prototype = {
+      logCalled: function() {
+        this.called = true;
+      }
+    };
+    var Construct = newless(BareConstructor);
+    var obj = Construct();
+
+    expect(Object.getPrototypeOf(obj)).to.equal(Construct.prototype);
+    expect(Object.getPrototypeOf(obj)).to.equal(BareConstructor.prototype);
+  });
+
+  it("should preserve a constructor's name", function() {
+    var Construct = newless(function Maker() {});
+    expect(Construct.name).to.equal("Maker");
+  });
+
+  it("should preserve a constructor's displayName", function() {
+    var BareConstructor = function() {};
+    BareConstructor.displayName = "Maker Constructor";
+    var Construct = newless(BareConstructor);
+    expect(Construct.displayName).to.equal("Maker Constructor");
+  });
+});
